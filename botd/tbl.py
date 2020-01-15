0155514727f0868e427ac9ff07f920687c57a33a
Bart Thate <bthate@dds.nl>
2020-01-15 15:48:40 +0100

    soook

--- botd/tbl.py
+++ botd/tbl.py
@@ -3,15 +3,15 @@
 # dispatch tables.
 
 modules = {
-    "cfg": "botd.cmds.cfg",
-    "cmds": "botd.cmds.cmd",
-    "flt": "botd.cmds.shw",
-    "log": "botd.cmds.ent",
-    "meet": "botd.cmds.usr",
-    "pid": "botd.cmds.shw",
-    "ps": "botd.cmds.thr",
-    "todo": "botd.cmds.ent",
-    "u": "botd.cmds.usr",
-    "up": "botd.cmds.shw",
-    "v": "botd.cmds.shw"
+    "cfg": "botd.cfg",
+    "cmds": "botd.cmd",
+    "flt": "botd.shw",
+    "log": "botd.ent",
+    "meet": "botd.usr",
+    "pid": "botd.shw",
+    "ps": "botd.shw",
+    "todo": "botd.ent",
+    "users": "botd.usr",
+    "up": "botd.shw",
+    "v": "botd.shw"
 }
