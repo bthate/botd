54174a023c1350dbef7f063e68d368feb297ef4a
Bart Thate <bthate@dds.nl>
2020-01-15 10:15:57 +0100

    add tbl

---
+++ botd/tbl.py
@@ -0,0 +1,17 @@
+# BOTD - python3 IRC channel daemon
+#
+# dispatch tables.
+
+modules = {
+    "cfg": "botd.cmds.cfg",
+    "cmds": "botd.cmds.cmd",
+    "flt": "botd.cmds.shw",
+    "log": "botd.cmds.ent",
+    "meet": "botd.cmds.usr",
+    "pid": "botd.cmds.shw",
+    "ps": "botd.cmds.thr",
+    "todo": "botd.cmds.ent",
+    "u": "botd.cmds.usr",
+    "up": "botd.cmds.shw",
+    "v": "botd.cmds.shw"
+}
